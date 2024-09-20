45c35f4ab37be9a572d617d9c3d89ff7c6eb79ca
Mohan <117554773+immoh4n@users.noreply.github.com>
2024-09-20 22:28:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,28 @@ const poolData = {
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-// Check if user is logged in
+// Check if the user is logged in
 const cognitoUser = userPool.getCurrentUser();
 
 if (cognitoUser) {
+    console.log("User found: ", cognitoUser);
     cognitoUser.getSession(function(err, session) {
         if (err) {
-            console.log(err);
+            console.error("Error getting session: ", err);
             return;
         }
 
         if (session.isValid()) {
+            console.log("Session is valid. Fetching user attributes...");
+            
             // Get user attributes
             cognitoUser.getUserAttributes(function(err, attributes) {
                 if (err) {
-                    console.log(err);
+                    console.error("Error fetching attributes: ", err);
                 } else {
+                    console.log("Attributes fetched: ", attributes);
                     attributes.forEach(attr => {
+                        console.log(attr.getName(), attr.getValue()); // Debug attribute names and values
                         if (attr.getName() === 'name') {
                             const userName = attr.getValue();
                             document.getElementById('user-name').innerText = `Welcome, ${userName}`;
@@ -30,6 +35,8 @@ if (cognitoUser) {
                     });
                 }
             });
+        } else {
+            console.log("Session is not valid.");
         }
     });
 } else {
